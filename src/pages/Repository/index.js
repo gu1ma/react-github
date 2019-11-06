3c7e1e07de8b4b9a76fd5820dbb313379179c58e
Gabriel Guimarães <gabrielgs1408@gmail.com>
2019-11-06 17:17:47 -0300

    [Add] Buscando dados da API

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return <h1>Repository {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends Component {
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      axios.get(`https://api.github.com/repos/${repoName}`),
+      axios.get(`https://api.github.com/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+
+    return <h1>Repository</h1>;
+  }
 }
